7cdcda2800d273820416237d37817206bd0aa47b
podnes <podnestrade@gmail.com>
2023-01-18 15:14:28 +0200

    improved mobile view

--- public/js/goals.js
+++ public/js/goals.js
@@ -1,17 +1,20 @@
 const wrapper = document.querySelector('.goals-overview');
 const goals = wrapper.querySelectorAll('.item');
 
-window.addEventListener('scroll', () => {
 
-    goals.forEach((goal,i ) => {
-        let value = wrapper.getBoundingClientRect().top
-        if(value > 0 && i !== 1){
-          goal.style.transform = `translateX(${value * 2.2}px)`
-        } else if(i === 1 && value > 0){
-          goal.style.transform = `translateX(-${value * 2.2}px)`
-        } else {
-            goal.style.transform = `translateX(0)`
-        }
-    })
 
+window.addEventListener('scroll', (e) => {
+    if(window.innerWidth > 992) {
+        goals.forEach((goal, i) => {
+            let value = wrapper.getBoundingClientRect().top
+            console.log(value);
+            if (value > 0 && i !== 1) {
+                goal.style.transform = `translateX(${value * 2.2}px)`
+            } else if (i === 1 && value > 0) {
+                goal.style.transform = `translateX(-${value * 2.2}px)`
+            } else {
+                goal.style.transform = `translateX(0)`
+            }
+        })
+    }
 })
--- public/js/stepper.js
+++ public/js/stepper.js
@@ -1,18 +1,25 @@
 const stepperItems = document.querySelectorAll('.c-stepper__item');
 const contentItems = document.querySelectorAll('.step-content .item');
-
+const descs = document.querySelectorAll('.c-stepper__desc');
 
 let stepperIdx = 0
 stepperItems.forEach((item, i) => {
     item.addEventListener('click', () => {
-        // contentItems[stepperIdx].style.animation = 'slideLeft 0.5s ease-in-out;'
-        contentItems[stepperIdx].classList.remove('active');
         stepperItems[stepperIdx].classList.remove('active');
+        descs[stepperIdx].classList.remove('active');
+        contentItems[stepperIdx].classList.remove('active');
 
 
+        if(i === 0) {
+            contentItems[i].style.animation = 'slideLeft 0.4s';
+        } else {
+            contentItems[i].style.animation = 'slideRight 0.4s';
+        }
         item.classList.add('active');
+        descs[i].classList.add('active');
         contentItems[i].classList.add('active');
         stepperIdx = i;
+
 
     });
 })
