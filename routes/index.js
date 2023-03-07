35580dd2902ad2c0ad7b19d339ce1bcf31ce317f
podnes <podnestrade@gmail.com>
2023-03-07 18:43:15 +0200

    cleaning up

--- routes/index.js
+++ routes/index.js
@@ -5,20 +5,10 @@ const express = require('express');
 const router = express.Router();
 
 
-router.get('/s', (req, res) => {
-    res.render('developing', {
-      pageTitle: 'Ace Battle Association',
-      path: '/',
-      theme: 'dark',
-      introImage: '/img/main-logo.png',
-    });
-})
 router.get('/', (req, res) => {
   res.render('index', {
     pageTitle: 'ABA - main',
     path: '/',
-    theme: 'dark',
-    introImage: '/img/main-logo.png',
   });
 });
 
@@ -26,8 +16,6 @@ router.get('/aba-structure', (req, res) => {
   res.render('aba-structure', {
     pageTitle: 'ABA - structure',
     path: '/aba-structure',
-    theme: 'dark',
-    introImage: '/img/main-logo.png',
   });
 })
 
@@ -35,8 +23,6 @@ router.get('/rules', (req, res) => {
   res.render('rules', {
     pageTitle: 'ABM - rules',
     path: '/rules',
-    theme: 'dark',
-    introImage: '/img/rules-abm-logo.png',
   });
 })
 
@@ -44,8 +30,6 @@ router.get('/about', (req, res) => {
   res.render('about', {
     pageTitle: 'ABM - about',
     path: '/about',
-    theme: 'dark',
-    introImage: '/img/logo-about-dark.png',
   });
 })
 
@@ -53,8 +37,6 @@ router.get('/competitions', (req, res) => {
   res.render('competitions', {
     pageTitle: 'ABM - Competitions',
     path: '/competitions',
-    theme: 'dark',
-    introImage: '/img/about-logo.png',
   });
 })
 
@@ -62,8 +44,6 @@ router.get('/competitions/cup-benelux', (req, res) => {
   res.render('cup-benelux', {
     pageTitle: 'ABM - Competitions',
     path: '/competitions',
-    theme: 'dark',
-    introImage: '/img/about-logo.png',
   });
 })
 
@@ -71,8 +51,6 @@ router.get('/news', (req, res) => {
   res.render('news', {
     pageTitle: 'ABA - news',
     path: '/news',
-    theme: 'dark',
-    introImage: '/img/about-logo.png',
   });
 })
 
@@ -80,8 +58,6 @@ router.get('/results', (req, res) => {
   res.render('results', {
     pageTitle: 'ABA - results and statistics',
     path: '/results',
-    theme: 'dark',
-    introImage: '/img/about-logo.png',
   });
 })
 
